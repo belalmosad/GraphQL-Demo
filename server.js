ee73054998e83c8ce616d8a0541d6753babb74b6
unknown <belalelmeligy516@gmail.com>
2022-07-03 11:56:04 +0200

    added posts array as a dummy Database

--- server.js
+++ server.js
@@ -1,6 +1,30 @@
 const { ApolloServer, gql } = require("apollo-server-express");
 const fs = require("fs");
-const typeDefs = gql
+const path = require("path");
+
+const schema = fs.readFileSync(path.join(__dirname + "typeDefs.graphql"));
+const typeDefs = gql(schema);
+
+const posts = [
+    {
+        id: 1,
+        title: 'post 1',
+        content: 'post 1 content'
+
+    },
+    {
+        id: 2,
+        title: 'post 2',
+        content: 'post 2 content'
+
+    },
+    {
+        id: 3,
+        title: 'post 3',
+        content: 'post 3 content'
+
+    },
+]
 
 const resolvers = {
     Query: {
