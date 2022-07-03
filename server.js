fabef2390186099c682176d1540845ce39b9352b
unknown <belalelmeligy516@gmail.com>
2022-07-03 11:48:39 +0200

    Added typeDefs file

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
-const { ApolloServer } = require("apollo-server-express");
+const { ApolloServer, gql } = require("apollo-server-express");
+const fs = require("fs");
+const typeDefs = gql
 
+const resolvers = {
+    Query: {
+
+    }
+}
 const server = new ApolloServer({
     resolvers,
     typeDefs
