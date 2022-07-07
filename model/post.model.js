ecfa43da1aac6cb1bfff84c1ec4cde943d5b6248
unknown <belalelmeligy516@gmail.com>
2022-07-08 01:47:50 +0200

    Added post model

---
+++ model/post.model.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+const postSchema = new mongoose.Schema(
+  {
+    title: String,
+    text: String,
+  }
+);
+
+module.exports = mongoose.model("posts", postSchema);
