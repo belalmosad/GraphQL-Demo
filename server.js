ee9d308554ea6d2d48c4a45b5f6f55a44c91819e
unknown <belalelmeligy516@gmail.com>
2022-07-08 04:13:54 +0200

    Added datasources to apollo server and Added Database connection to rely on MongoDB instead of in-memory operations

--- server.js
+++ server.js
@@ -1,71 +1,23 @@
 const {ApolloServer, gql} = require('apollo-server');
 const fs = require("fs");
-const path = require("path");
+const CommentDataSource = require('./datasource/comment.datasource');
+const PostDataSource = require('./datasource/post.datasource');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://admin:123@cluster0.mawss.mongodb.net/?retryWrites=true&w=majority')
+.then(() => {console.log('DB Connected Successfully')})
+.catch(error => {console.log(error)});
 
 let  schema = fs.readFileSync('./typeDefs.graphql', {encoding: 'utf-8'});
 const typeDefs = gql(schema);
 
-const posts = [
-    {
-        id: 1,
-        title: 'post 1',
-        content: 'post 1 content',
-        comments: [{
-            postId: 1,
-            username: "belal"
-        }]
-    },
-    {
-        id: 2,
-        title: 'post 2',
-        content: 'post 2 content',
-        comments: [{
-            postId: 2,
-            username: "belal"
-        }]
-    },
-    {
-        id: 3,
-        title: 'post 3',
-        content: 'post 3 content',
-        comments: [{
-            postId: 3,
-            username: "belal"
-        }]
-    },
-]
-
-const resolvers = {
-    Query: {
-        post: (_, args) => {return posts.find((post) => {return post.id == args.id})},
-        posts : () => {return posts}
-    },
-    Mutation: {
-        addPost: (_, args) => {
-            let newPost = {id: args.id, title: args.title, content: args.content}
-            posts.push(newPost);
-            return newPost
-        }, 
-        deletePost: (_, args) => {
-            return posts.filter((post) => {return post.id != args.id})
-        },
-        updatePost: (_, args) => {
-            let tobeUpdated = posts.findIndex((post) => post.id == args.id);
-            let newpost = {id: args.id, title: args.title, content: args.content}
-            posts[tobeUpdated] = newpost
-            return newpost
-        },
-        addComment: (_, args) => {
-            let newComment = {postId: args.postId, username: args.username}
-            let postIndex = posts.findIndex((post) => post.id == args.postId);
-            posts[postIndex].comments.push(newComment);
-            return newComment;
-        }
-    }
-}
 const server = new ApolloServer({
     resolvers,
-    typeDefs
+    typeDefs,
+    dataSources: () => ({
+        post: new PostDataSource,
+        comment: new CommentDataSource
+    })
 })
 
 server
