56eefa7153ec5c82b2ca1b215392f230fb605345
unknown <belalelmeligy516@gmail.com>
2022-07-08 04:08:07 +0200

    added index.js to export all resolvers

---
+++ resolvers/index.js
@@ -0,0 +1,11 @@
+const Query = require('./query');
+const Mutation = require('./mutation');
+const Post = require('./post');
+const Comment = require('./comment');
+
+module.exports = {
+  Query,
+  Mutation,
+  Post,
+  Comment,
+};
