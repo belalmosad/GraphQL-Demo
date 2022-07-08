dee0141dea440787604cbef73d3fcc5f68800d21
unknown <belalelmeligy516@gmail.com>
2022-07-08 18:51:36 +0200

    Changed Id type to string

--- model/post.model.js
+++ model/post.model.js
@@ -3,8 +3,8 @@ const mongoose = require("mongoose");
 const postSchema = new mongoose.Schema(
   {
     id: {
-        type: Number,
-        unique: true
+        type: String,
+        uniqie: true
     },
     title: String,
     content: String,
