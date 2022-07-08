66b54b87596401054e9b6f786314b8194155beb5
unknown <belalelmeligy516@gmail.com>
2022-07-08 04:03:35 +0200

    Implemented CRUD on Comments in mutation resolver

--- resolvers/mutation.js
+++ resolvers/mutation.js
@@ -2,15 +2,27 @@ const Mutation = {
 
     // Post CRUD operations
     addPost: (_, input, context) => {
-      return context.dataSources.post.create(input);
+        return context.dataSources.post.create(input);
     },
 
-    deletePost: (_, {postId}, context) => {
+    deletePost: (_, { postId }, context) => {
         return context.dataSources.post.deletePost(postId)
     },
 
-    updatePost: (_, {postId, data}, context) => {
+    updatePost: (_, { postId, data }, context) => {
         return context.dataSources.post.updatePost(postId, data);
     },
-  };
-  module.exports = Mutation;
+
+    // Comment CRUD operations
+    addComment: (_, input, context) => {
+        return context.dataSources.comment.create(input);
+    },
+    deleteComment: (_, {commentId, postId}, context) => {
+        return context.dataSources.comment.deleteComment(commentId, postId);
+    },
+    updateComment: (_, {commentId, postId, data}, context) => {
+        return context.dataSources.comment.updateComment(commentId, postId, data)
+    }
+
+};
+module.exports = Mutation;
